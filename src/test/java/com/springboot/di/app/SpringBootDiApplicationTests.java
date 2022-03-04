fa3c89a0cbaed408b89337b2e1978d5e248393c7
Francisco Bañuelos <franciscobanuelosvaladez@gmail.com>
2022-03-04 11:14:45 -0600

    Inyeccion de Dependencias

---
+++ src/test/java/com/springboot/di/app/SpringBootDiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.springboot.di.app;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
